89d70d61d7d25e2a1ca38faa9876c7c7f6fa6b54
Bemnet57 <bemnetasseged57@gmail.com>
2026-06-15 04:03:39 +0300

    clean endpoint

--- contact-generator/src/main/java/com/example/contact_generator/controller/EmployeeController.java
+++ contact-generator/src/main/java/com/example/contact_generator/controller/EmployeeController.java
@@ -1,10 +1,8 @@
 package com.example.contact_generator.controller;
 
 import com.example.contact_generator.dto.EmployeeResponseDTO;
-import com.example.contact_generator.entity.Employee;
-import com.example.contact_generator.qr.QRCodeService;
+import com.example.contact_generator.service.ContactQrService;
 import com.example.contact_generator.service.EmployeeService;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.contact_generator.qr.VCardService;
 
 import java.util.List;
 
@@ -22,13 +19,14 @@ import java.util.List;
 public class EmployeeController {
 
     private final EmployeeService employeeService;
-    private final VCardService vCardService;
-    private final QRCodeService qrCodeService;
+    private final ContactQrService contactQrService;
 
-    public EmployeeController(EmployeeService employeeService,VCardService vCardService,QRCodeService qrCodeService) {
+    public EmployeeController(
+            EmployeeService employeeService,
+            ContactQrService contactQrService) {
+
         this.employeeService = employeeService;
-        this.vCardService = vCardService;
-        this.qrCodeService = qrCodeService;
+        this.contactQrService = contactQrService;
     }
 
     //Get All Employees Endpoint
@@ -39,7 +37,7 @@ public class EmployeeController {
 
     //Search By Name Endpoint
     @GetMapping("/search")
-    public List<Employee> searchEmployees(@RequestParam String name) {
+    public List<EmployeeResponseDTO> searchEmployees(@RequestParam String name) {
         return employeeService.searchEmployees(name);
     }
 
@@ -51,61 +49,17 @@ public class EmployeeController {
         return employeeService.getEmployeeById(id);
     }
 
-    //just to test vcard generation
-    @GetMapping("/{id}/vcard")
-    public String getVCard(@PathVariable Long id) {
-
-        Employee employee =
-                employeeService.getEmployeeEntityById(id);
-
-        return vCardService.buildVCard(employee);
-    }
-
-    @GetMapping("/test-qr")
-    public ResponseEntity<byte[]> testQr() {
-
-        byte[] qrCode =  qrCodeService.generateQRCode("Hello World");
-
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.IMAGE_PNG_VALUE)
-                .body(qrCode);
-    }
-
-    @GetMapping("/test-vcard-qr")
-    public ResponseEntity<byte[]> testVCardQr() {
-
-        String vCard =
-                """
-                BEGIN:VCARD
-                VERSION:3.0
-                FN:John Doe
-                TEL;TYPE=CELL:+251911223344
-                EMAIL:john@example.com
-                END:VCARD
-                """;
+    @GetMapping("/{id}/qrcode")
+    public ResponseEntity<byte[]> getEmployeeQrCode(
+            @PathVariable Long id) {
 
         byte[] qrCode =
-                qrCodeService.generateQRCode(vCard);
+                contactQrService.generateEmployeeQr(id);
 
         return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_TYPE,
-                        MediaType.IMAGE_PNG_VALUE)
+                .contentType(MediaType.IMAGE_PNG)
                 .body(qrCode);
-    }
 
-    // QR Endpoint
-    @GetMapping("/{id}/qrcode")
-    public ResponseEntity<byte[]> getEmployeeQrCode(@PathVariable Long id) {
-
-        Employee employee = employeeService.getEmployeeEntityById(id);
-
-        String vCard = vCardService.buildVCard(employee);
-
-        byte[] qrCode = qrCodeService.generateQRCode(vCard);
-
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.IMAGE_PNG_VALUE)
-                .body(qrCode);
     }
 
 
